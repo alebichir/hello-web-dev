9e8a8fb25bd6a406f386df974320ad63f42542f6
Alexandra <alexandrabichir@yahoo.com>
2017-09-12 21:51:24 +0300

    shuffled numbers

--- javascriptfile.js
+++ javascriptfile.js
@@ -1,14 +1,42 @@
-//window.onload = newTable;
-
 function newTable() {
     for (var i=0; i<16; i++) {
        setSquare(i);
     }
 }
 
-function setSquare(thisSquare) {
+function setSquare(thisSquare, entry) {
     var currSquare = "square" + thisSquare;
-    var newNum = Math.floor(Math.random() * 15) + 1;
-
-    document.getElementById(currSquare). innerHTML = newNum;
+    document.getElementById(currSquare).innerHTML = entry;
 }
+
+for (var numbers = [], i = 0; i < 15; ++i) {
+    numbers[i] = i;
+}
+
+console.warn(numbers);
+
+function shuffle(array) {
+    var tmp, current, top = array.length;
+    if(top) while(--top) {
+        current = Math.floor(Math.random() * (top + 1));
+        tmp = array[current];
+        array[current] = array[top];
+        array[top] = tmp;
+    }
+    return array;
+}
+
+var shuffledNumbers = shuffle(numbers);
+
+for (var i = 0; i < shuffledNumbers.length; i++) {
+    var entry = shuffledNumbers[i];
+    setSquare(i, entry);
+}
+
+
+$('table td').click(function () {
+    var nr = this.innerHTML;
+    var id = this.id.replace('square', '');
+    console.info("Handler for .click() called.", this, id, nr);
+
+});
