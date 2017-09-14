3029407665a9d0e26a158cf4dca9e4466d5ac56b
Alexandra <alexandrabichir@yahoo.com>
2017-09-14 23:58:29 +0300

    find neighbor cells, move numbers into the empty cell, a little bit of CSS

--- javascriptfile.js
+++ javascriptfile.js
@@ -13,7 +13,7 @@ for (var numbers = [], i = 0; i < 15; ++i) {
     numbers[i] = i;
 }
 
-console.warn(numbers);
+//console.warn(numbers);
 
 function shuffle(array) {
     var tmp, current, top = array.length;
@@ -34,9 +34,32 @@ for (var i = 0; i < shuffledNumbers.length; i++) {
 }
 
 
-$('table td').click(function () {
+$('#random-numbers td').click(function () {
     var nr = this.innerHTML;
     var id = this.id.replace('square', '');
-    console.info("Handler for .click() called.", this, id, nr);
+    console.info("Clicked cell", this, id, nr);
+
+    var $td = $(this);
+    var index = $td.index(), $tr =$td.parent();
+    var $nbrs = $td.prev(); //find the previous td
+    $nbrs = $nbrs.add($td.next());//find the next td
+    $nbrs = $nbrs.add($tr.prev().find('td').eq(index));//find the td with the same index in previous row
+    $nbrs = $nbrs.add($tr.next().find('td').eq(index));//find the td with the same index in next row
+    console.log($nbrs.get());
+
+if (nr === '') {
+    console.info("Clicked cell is empty", this, id, nr);
+    } else {
+    console.info("Clicked cell is not empty", this, id, nr);
+};
+
+$("td:empty").text(nr);
+$(this).text('');
 
 });
+
+
+
+
+
+
